dcdeb8c06d0ac3194a7c3d1e07acbc65b48dfbeb
niadulMunna <niadulislammunna@gmail.com>
2025-01-07 23:17:07 +0600

    icon image

---
+++ src/app/data/autoSlider.tsx
@@ -0,0 +1,11 @@
+import Araal from "@/app/assets/CHORKI FLICK/thumbnails_Araal.webp";
+import Bokul from "@/app/assets/CHORKI FLICK/thumbnails_Bokul_ful.webp";
+import JoarBhata from "@/app/assets/CHORKI FLICK/thumbnails_Joar_bhata.webp";
+import Othithi from "@/app/assets/CHORKI FLICK/thumbnails_Otithi.webp";
+
+export const Data = [
+  { Poster: Araal },
+  { Poster: Bokul },
+  { Poster: JoarBhata },
+  { Poster: Othithi },
+];
---
+++ src/components/index.tsx
@@ -0,0 +1,19 @@
+// import {
+//   PosterAgamikal,
+//   //   Poster_DhakaWar,
+//   //   Poster_EtiChitra,
+//   //   Poster_BlackWar,
+//   //   Poster_JaoPakhiBoloTare,
+//   //   Poster_Local,
+// } from "@/app/assets/FULL-ON FILMY/Poster_Agamikal.webp";
+
+// export const feedback = [
+//   {
+//     id: "feedback-1",
+//     content:
+//       "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+//     name: "Herman Jensen",
+//     title: "Founder & Leader",
+//     img: PosterAgamikal,
+//   },
+// ];
